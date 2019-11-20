fbc1805bb276ab138b70a7a3bb9bad7eb48d53bd
athena314320 <55180760+athena314320@users.noreply.github.com>
2019-11-20 15:52:53 +0800

    1120

---
+++ Projectohhhhh/Assets/script/LearnMethod.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    public void Drive(int SPEED)
+    {
+        print("音效~");
+        print("開車，時速" + SPEED);
+
+    }
+
+    public void Start()
+    {
+        print("哈瞜~沃德");
+        Drive(200);
+        Drive(300);
+        Drive(400);
+    }
+
+
+
+}
--- Projectohhhhh/Assets/script/chicken.cs
+++ Projectohhhhh/Assets/script/chicken.cs
@@ -2,9 +2,11 @@
 
 public class chicken : MonoBehaviour
 {
-    int speed = 10;
-    float turn = 20.5f;
-    bool mission;
-    string name = "";
+    [Header("移動速度")] [Range(1, 100)]
+    public int speed = 10;
+    [Tooltip("G8雞的旋轉速度")]
+    public float turn = 20.5f;
+    public bool mission;
+    public string name = "G8雞";
 
 }
